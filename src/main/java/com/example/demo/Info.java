416c3dd8921b8d318e7af198f432024644e68878
Vadim Bobylov <davyjonessss@ukr.net>
2021-11-28 19:43:58 +0200

    first commit

---
+++ src/main/java/com/example/demo/Info.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Info {
+    String systemIP;
+    List<String> words;
+}
---
+++ src/main/java/com/example/demo/KubernetesController.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import com.github.javafaker.Faker;
+import lombok.SneakyThrows;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class KubernetesController {
+    List<String> words = new ArrayList<>();
+
+    @GetMapping
+    @SneakyThrows
+    public Info getInfo() {
+        words.add(new Faker().lorem().sentence(4));
+        return new Info(InetAddress.getLocalHost().toString(), words);
+    }
+}
